517560d98618684cae72336e6902b1ca21072c9e
edwardz10 <edward.ziyangulov@gmail.com>
2018-07-18 22:43:15 +0300

    added 2 more standards

--- src/main/kotlin/com/jazz/duet/standards/service/DataLoaderService.kt
+++ src/main/kotlin/com/jazz/duet/standards/service/DataLoaderService.kt
@@ -15,7 +15,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("Eb |Eb |Dm7b5 |G7 |Cm7 |Cm7 |Bbm7 |Eb7 " +
                 "|Ab |Db7 |Eb |Cm7 |F7 |F7 |Fm7 |Bb7 " +
                 "|Eb |Eb |Dm7b5 |G7 |Cm7 |Cm7 |Bbm7 |Eb7 " +
-                "|Ab |Db7 |Eb |Am7b5 D7 |Gm7 Ab|Gm7 C7 |Fm7 B7 |Eb|",
+                "|Ab |Db7 |Eb |Am7b5 D7 |Gm7 Ab|Gm7 C7 |Fm7 B7 |Eb",
                 "Eb",
                 "There Will Never Be Another You",
                 "https://www.youtube.com/watch?v=lkB9w50wGPI",
@@ -33,7 +33,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("G6/9 |G6/9 |G#6/9 |G#6/9 |G6/9 |G6/9 |E7 |E7 " +
                 "|A7 |A7 |D7 |D7 |G/B |BbO |Am7 |D7 " +
                 "|G6/9 |G6/9 |G#6/9 |G#6/9 |G6/9 |G6/9 |E7 |E7 " +
-                "|Am6 |Cm6 |G/B |E7 |Am7 |D7 |G G#O |Am7 D7|",
+                "|Am6 |Cm6 |G/B |E7 |Am7 |D7 |G G#O |Am7 D7",
                 "G",
                 "Hungaria",
                 "https://www.youtube.com/watch?v=l_HKqgi32Rw",
@@ -42,7 +42,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("F |F |F |F |F |F |D7 |D7 " +
                 "|G7 |G7 |G7 |G7 |Bbm6 |Bbm6 |C7 |Eb7" +
                 "|Ab |Eb7 |Ab |Eb7 |Ab |Eb7 |Ab |C7 " +
-                "|F |F |F#O |F#O |Gm7 |C7 |F F#O |Gm7 C7|",
+                "|F |F |F#O |F#O |Gm7 |C7 |F F#O |Gm7 C7",
                 "F",
                 "China Boy",
                 "https://www.youtube.com/watch?v=bQeOPDvrAN4",
@@ -51,7 +51,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("G7 |C7 |F |F |F7 |F7 |Bb |Bb " +
                 "|Bbm6 |Bbm6 |F |D7 |G7 |G7 |C7 |C7" +
                 "|G7 |C7 |F |F |F7 |F7 |Bb |Bb " +
-                "|Bbm6 |Bbm6 |F |D7 |G7 |C7 |Am7 |D7|",
+                "|Bbm6 |Bbm6 |F |D7 |G7 |C7 |Am7 |D7",
                 "F",
                 "Rose Room",
                 "https://www.youtube.com/watch?v=bQeOPDvrAN4",
@@ -60,7 +60,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("A |A |A |A |A/C# |CO |E7/B |E7 " +
                 "|E7 |E7 |F7 |F7 |A/C# |CO |Bm7 |E7" +
                 "|A |A |A |A |Em7 |A7 |D |D " +
-                "|D |D#O |A7 |B7 |E7 |A CO |Bm E7|",
+                "|D |D#O |A7 |B7 |E7 |A CO |Bm E7",
                 "A",
                 "Django|s Tiger",
                 "https://www.youtube.com/watch?v=AQW2RxwwnRo",
@@ -69,7 +69,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("C7 |C7 |C7 |C7 |A7 |A7 |A7 |A7 " +
                 "|G |G |B7 |Em |A7 |A7 |D7 |Db7" +
                 "|C7 |C7 |C7 |C7 |A7 |A7 |A7 |A7 " +
-                "|G |E7 |Am |Am |Cm6 |D7 |G |G7|",
+                "|G |E7 |Am |Am |Cm6 |D7 |G |G7",
                 "C",
                 "Limehouse Blues",
                 "https://www.youtube.com/watch?v=nr47ZhrNfUY",
@@ -78,7 +78,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("C |C |Bb7 |A7 |Dm7 |A7 |Dm7 |Dm7 " +
                 "|G7 |G7 |D7 |G7 |C/E |EbO |Dm7 |G7" +
                 "|C |C |Bb7 |A7 |Dm7 |A7 |Dm7 |Dm7 " +
-                "|F |F#O |C |A7 |Dm7 |G7 |C EbO |Dm G7|",
+                "|F |F#O |C |A7 |Dm7 |G7 |C EbO |Dm G7",
                 "C",
                 "My Melancholy Baby",
                 "https://www.youtube.com/watch?v=alEi3HUNd2E",
@@ -86,7 +86,7 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
 
         saveSheetAndStandard("F |Emb5 A7b9 |Dm7 G7 |Cm7 F7 " +
                 "|Bb |Bbm7 Eb7 |Am7 D7 |Abm7 Dbm7 " +
-                "|Gm7 |C7 |F Dm7 |Gm7 C7|",
+                "|Gm7 |C7 |F Dm7 |Gm7 C7",
                 "F",
                 "Blues For Alice",
                 "https://www.youtube.com/watch?v=UtdzMfSwXi8",
@@ -95,11 +95,30 @@ class DataLoaderService(val standardsRepository: StandardsRepository, val sheets
         saveSheetAndStandard("G |G |Gm7 |C7 |F |F |Fm7 |Bb7 " +
                 "|Eb7 |Am7b5 D7 |Gm7 |D7+9 |Bm7 |E7 |Am7 |D7" +
                 "|G |G |Gm7 |C7 |F |F |Fm7 |Bb7 " +
-                "|Eb7 |Am7b5 D7 |G |G |Bm7 E7|Am7 D7|G E7 |Am7 D7|",
+                "|Eb7 |Am7b5 D7 |G |G |Bm7 E7|Am7 D7|G E7 |Am7 D7",
                 "G",
                 "Ornithology",
                 "https://www.youtube.com/watch?v=QfuCZQagz2A",
                 "C. Parker")
+
+        saveSheetAndStandard("Bb |Bb |Bbm7 |Bbm7 |F |E7 |F |F " +
+                "|D7 |D7 |D7 |D7 |G7 |G7 |Gm7 |C7 " +
+                "|Bb |Bb |Bbm7 |Bbm7 |F |E7 |F |F " +
+                "|D7 |D7 |A7 |Dm7 |Bb BO|C7 |F |F7",
+                "F",
+                "I'll See You In My Dreams",
+                "https://www.youtube.com/watch?v=zlu1HS_pQp8",
+                "I. Jones")
+
+        saveSheetAndStandard("Gm7 |Gm7 |Eb7 D7 |Gm7 |C7 |F7 |Bb |D7+ " +
+                "|Gm7 |Gm7 |Eb7 D7 |Gm7 |C7 |F7 |Bb |Bb " +
+                "|Fm7 |Bb7 |Eb |Eb |Gm7 |C7 |F |D7 " +
+                "|Gm7 |Gm7 |Eb7 D7 |Gm7 |C7 |F7 |Bb |D7+",
+                "Gm",
+                "It Don't Mean A Thing",
+                "https://www.youtube.com/watch?v=jvxPVK4ISVk",
+                "D. Ellington")
+
     }
 
     fun saveSheetAndStandard(chords: String, key: String, name: String, video: String, author:
