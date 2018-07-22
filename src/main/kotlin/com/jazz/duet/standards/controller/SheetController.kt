acd07f91f05d9958abb2f85c316231ddb033bf19
edwardz10 <edward.ziyangulov@gmail.com>
2018-07-22 15:49:45 +0300

    Added SheetController + SheetService

--- src/main/kotlin/com/jazz/duet/standards/controller/SheetController.kt
+++ src/main/kotlin/com/jazz/duet/standards/controller/SheetController.kt
@@ -1,4 +1,40 @@
 package com.jazz.duet.standards.controller
 
-class SheetController {
+import com.google.common.collect.FluentIterable
+import com.jazz.duet.standards.model.Sheet
+import com.jazz.duet.standards.service.SheetsService
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.validation.constraints.NotNull
+
+@RestController
+@RequestMapping(
+        value = ["/api/sheets"])
+class SheetController(val sheetsService: SheetsService) {
+
+    @GetMapping(value = "/all")
+    fun allSheets(): List<Sheet> =
+            FluentIterable.from(sheetsService.allSheets()).toList()
+
+    @GetMapping(value = "/{id}")
+    fun getSheetById(@NotNull @PathVariable("id")
+                     id: Long): Optional<Sheet> =
+            sheetsService.getSheetById(id)
+
+    @PostMapping(value = "/new", consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun createSheet(@NotNull @RequestBody Sheet: Sheet): Sheet =
+            sheetsService.createSheet(Sheet)
+
+    @PutMapping(value = "/{id}", consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun updateSheet(@NotNull @PathVariable("id")
+                    id: Long,
+                    @NotNull @RequestBody Sheet: Sheet): Sheet =
+            sheetsService.updateSheet(id, Sheet)
+
+    @DeleteMapping(value = "/{id}")
+    fun deleteSheet(@NotNull
+                    @PathVariable("id")
+                    id: Long) =
+            sheetsService.deleteSheet(id)
 }
--- src/main/kotlin/com/jazz/duet/standards/service/SheetsService.kt
+++ src/main/kotlin/com/jazz/duet/standards/service/SheetsService.kt
@@ -1,4 +1,39 @@
 package com.jazz.duet.standards.service
 
-class SheetsService {
+import com.jazz.duet.standards.model.Sheet
+import com.jazz.duet.standards.repository.SheetsRepository
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class SheetsService(val sheetsRepository: SheetsRepository) {
+
+    fun allSheets() : MutableIterable<Sheet> {
+        return sheetsRepository.findAll()
+    }
+
+    fun getSheetById(id: Long): Optional<Sheet> {
+        return sheetsRepository.findById(id)
+    }
+
+    fun createSheet(sheet: Sheet) : Sheet {
+        if (sheet.sheet_id != null) {
+            sheet.sheet_id = null
+        }
+
+        return sheetsRepository.save(sheet)
+    }
+
+    fun updateSheet(id: Long, sheet: Sheet): Sheet {
+        var oldSheet = getSheetById(id).get()
+        oldSheet.chords = sheet.chords
+        oldSheet.key = sheet.key
+
+        return sheetsRepository.save(oldSheet)
+    }
+
+    fun deleteSheet(id: Long) {
+        sheetsRepository.deleteById(id)
+    }
+
 }
